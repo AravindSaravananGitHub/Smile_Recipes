550b371a3258e8f05cfee3ef05091f602843385f
aravindprime123 <127675034+aravindprime123@users.noreply.github.com>
2023-03-12 20:32:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+-------------------
+Burgar Menu - Start
+-------------------
+*/
+const btn=document.getElementsByClassName('bars')[0];
+const nav=document.getElementsByClassName('nav-links')[0];
+
+btn.addEventListener('click',()=>{
+    nav.classList.toggle('prime');
+});
+/*
+-------------------
+Burgar Menu - End
+-------------------
+*/
